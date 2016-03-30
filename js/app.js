d63f7821de64bc7cd75960ad8f9ae68066e352c5
corbin737 <corbin737@me.com>
2016-03-30 11:30:24 -0600

    Add fading chevrons to dashboard menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
     // Hack to allow for full height while maintaining clearfix
     $('.dashboard-menu').height($('.dashboard-body').height());
+
+    // Fading chevrons on dashboard menu
+    $('.nav li:not(.active)').hover(function() {
+        $(this).find('.fade-on-hover').animate({
+            left: '0px',
+            opacity: '1'
+        }, 300);
+    }, function() {
+        $(this).find('.fade-on-hover').animate({
+            left: '4px',
+            opacity: '0'
+        }, 300);
+    }).click(function() {
+        $(this).find('.fade-on-hover').animate({
+            left: '-4px',
+            opacity: '0'
+        }, 300);
+    });
 });
